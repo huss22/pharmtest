19763acd9d032908534b53828c3d1c8afa4b1ebe
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-28 16:36:46 +1100

    Create retrievedata.js

---
+++ js/retrievedata.js
@@ -0,0 +1,29 @@
+var ref = new Firebase("https://boiling-heat-5681.firebaseio.com/");
+var postsRef = ref.child("Medicines");
+postsRef.on('child_added', function (snapshot) {
+  var newPost = snapshot.val();
+  var date = new Date(newPost.Timestamp);
+  $('#result').append("<option id =  " + newPost.UID + '>' + newPost.MedName + ', Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</option>');
+});
+
+function rerun (snapshot) {
+	var ref = new Firebase("https://boiling-heat-5681.firebaseio.com/");
+	var postsRef = ref.child("Medicines");
+  var newPost = snapshot.val();
+  var date = new Date(newPost.Timestamp);
+  $('#result').empty();
+  $('#result').append("<option></option>")
+  $('#result').append("<option id =  " + newPost.UID + '>' + newPost.MedName + ', Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</option>');
+}
+
+$(document).ready(function(){
+	$("#result").select2({
+    placeholder: "Select a Medicine",
+    width: "30%"
+});
+});
+
+$(document).on('change','#result', function(){
+	window.grabid = $("#result option:selected").attr("id");
+	console.log(grabid);
+});
