be11d873b825b1dc419d767bc6cb40ed08a68c28
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-28 18:20:18 +1100

    Update retrievedata.js

--- js/retrievedata.js
+++ js/retrievedata.js
@@ -3,7 +3,7 @@ var postsRef = ref.child("Medicines");
 postsRef.on('child_added', function (snapshot) {
   var newPost = snapshot.val();
   var date = new Date(newPost.Timestamp);
-  $('#result').append("<option id =  " + newPost.UID + '>' + newPost.MedName + ', Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</option>');
+  $('#result').append("<li id =  " + newPost.UID + '><b>' + newPost.MedName + '</b>, Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</li>');
 });
 
 function rerun (snapshot) {
