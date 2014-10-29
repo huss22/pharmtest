19dfb2cde61301d7c5630488b7e9d83fea2a79c8
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-29 16:37:04 +1100

    Update places.js

--- js/places.js
+++ js/places.js
@@ -3,7 +3,6 @@ var service;
 var infowindow;
 var x = document.getElementById("pharmacyresult");
 
-
 function initialize(position) {
   getLocation();
   var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
@@ -35,6 +34,7 @@ function createMarker(place) {
    $('#pharmacyresult').append('<br><b>Closest Pharmacy: </b>' + place.name + ", located at " + place.formatted_address);
    $('#pharmacyresult').append(place.opening_hours);
    console.log(place);
+   initializegp(position);
 }
 
 function getLocation() {
@@ -45,5 +45,73 @@ function getLocation() {
     }
 }
 
+
+function initializegp(position) {
+  getLocation();
+  var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: 'GP'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker(results[0]);
+  }
+}
+
+function createMarker(place) {
+  $('#gpresult').empty();
+   $('#gpresult').append('<br><b>Closest GP: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#gpresult').append(place.opening_hours);
+   console.log(place);
+   initializehospital(position);
+}
+
+function initializehospital(position) {
+  getLocation();
+  var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: 'Hospital'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker(results[0]);
+  }
+}
+
+function createMarker(place) {
+  $('#gpresult').empty();
+   $('#gpresult').append('<br><b>Closest Hospital: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#gpresult').append(place.opening_hours);
+   console.log(place);
+   initializehospital(position);
+}
 
 google.maps.event.addDomListener(window, 'load', getLocation);
