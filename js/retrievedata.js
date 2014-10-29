5f29dd727424f1afa8ee2edc481bdf6ab7a8cc84
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-29 17:05:41 +1100

    Update retrievedata.js

--- js/retrievedata.js
+++ js/retrievedata.js
@@ -5,24 +5,3 @@ postsRef.on('child_added', function (snapshot) {
   var date = new Date(newPost.Timestamp);
   $('#result').append("<li id =  " + newPost.UID + '><b>' + newPost.MedName + '</b>, Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</li>');
 });
-
-function rerun (snapshot) {
-	var ref = new Firebase("https://boiling-heat-5681.firebaseio.com/");
-	var postsRef = ref.child("Medicines");
-  var newPost = snapshot.val();
-  var date = new Date(newPost.Timestamp);
-  $('#result').empty();
-  $('#result').append("<li id =  " + newPost.UID + '><b>' + newPost.MedName + '</b>, Added on : ' + $.format.date(date, "ddd D of MMM, yyyy h:mm a") + '</li>');
-}
-
-$(document).ready(function(){
-	$("#result").select2({
-    placeholder: "Select a Medicine",
-    width: "30%"
-});
-});
-
-$(document).on('change','#result', function(){
-	window.grabid = $("#result option:selected").attr("id");
-	console.log(grabid);
-});
