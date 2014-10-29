94da71a6b1aba96911fa17c036f2418f01bb34bd
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-29 17:13:41 +1100

    Update places.js

--- js/places.js
+++ js/places.js
@@ -18,14 +18,28 @@ function initialize(position) {
     query: 'pharmacy'
   };
 
+  var request2 = {
+    location: pyrmont,
+    radius: '500',
+    query: 'GP'
+  };
+
   service = new google.maps.places.PlacesService(map);
   service.textSearch(request, callback);
+  service.textSearch(request2, callback2);
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
       var place = results[0];
       createMarker(results[0]);
+  }
+}
+
+function callback2(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker2(results[0]);
   }
 }
 
@@ -36,81 +50,19 @@ function createMarker(place) {
    console.log(place);
 }
 
+function createMarker2(place) {
+  $('#gpresult').empty();
+   $('#gpresult').append('<br><b>Closest Pharmacy: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#gpresult').append(place.opening_hours);
+   console.log(place);
+}
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(initialize);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
-}
-
-
-function initializegp(position) {
-  getLocation();
-  var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    query: 'GP'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-      var place = results[0];
-      createMarker(results[0]);
-  }
-}
-
-function createMarker(place) {
-  $('#gpresult').empty();
-   $('#gpresult').append('<br><b>Closest GP: </b>' + place.name + ", located at " + place.formatted_address);
-   $('#gpresult').append(place.opening_hours);
-   console.log(place);
-   initializehospital(position);
-}
-
-function initializehospital(position) {
-  getLocation();
-  var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    query: 'Hospital'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-      var place = results[0];
-      createMarker(results[0]);
-  }
-}
-
-function createMarker(place) {
-  $('#gpresult').empty();
-   $('#gpresult').append('<br><b>Closest Hospital: </b>' + place.name + ", located at " + place.formatted_address);
-   $('#gpresult').append(place.opening_hours);
-   console.log(place);
-   initializehospital(position);
 }
 
 google.maps.event.addDomListener(window, 'load', getLocation);
