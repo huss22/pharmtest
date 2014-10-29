c71ea60e73e6e08c2cb6d16531201eaf37319bc3
Hasnain Afzal <huss22@users.noreply.github.com>
2014-10-29 17:17:26 +1100

    Update places.js

--- js/places.js
+++ js/places.js
@@ -24,9 +24,16 @@ function initialize(position) {
     query: 'GP'
   };
 
+  var request3 = {
+    location: pyrmont,
+    radius: '500',
+    query: 'Hospital'
+  };
+
   service = new google.maps.places.PlacesService(map);
   service.textSearch(request, callback);
   service.textSearch(request2, callback2);
+  service.textSearch(request3, callback3);
 }
 
 function callback(results, status) {
@@ -43,6 +50,13 @@ function callback2(results, status) {
   }
 }
 
+function callback3(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker3(results[0]);
+  }
+}
+
 function createMarker(place) {
   $('#pharmacyresult').empty();
    $('#pharmacyresult').append('<br><b>Closest Pharmacy: </b>' + place.name + ", located at " + place.formatted_address);
@@ -52,8 +66,15 @@ function createMarker(place) {
 
 function createMarker2(place) {
   $('#gpresult').empty();
-   $('#gpresult').append('<br><b>Closest Pharmacy: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#gpresult').append('<br><b>Closest GP: </b>' + place.name + ", located at " + place.formatted_address);
    $('#gpresult').append(place.opening_hours);
+   console.log(place);
+}
+
+function createMarker3(place) {
+  $('#hospitalresult').empty();
+   $('#hospitalresult').append('<br><b>Closest Hospital: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#hospitalresult').append(place.opening_hours);
    console.log(place);
 }
 
